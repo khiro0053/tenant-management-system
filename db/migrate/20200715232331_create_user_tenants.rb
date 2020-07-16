cb08d88dae0274efd028bc455f33c8f6fd43832d
khiro <khiro0053@gmail.com>
2020-07-16 15:21:49 +0900

    bundle exec rails g model UserTenant user:references tenant:references

---
+++ db/migrate/20200715232331_create_user_tenants.rb
@@ -0,0 +1,10 @@
+class CreateUserTenants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_tenants do |t|
+      t.references :user, foreign_key: true
+      t.references :tenant, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
