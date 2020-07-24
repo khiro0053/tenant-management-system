ccbb5c68b06db7e014c1a47a2d3ecc0b9156f46f
khiro <khiro0053@gmail.com>
2020-07-24 19:51:56 +0900

    fix filename & column to execute rails db:migrate

--- db/migrate/20200717151209_create_rooms.rb
+++ db/migrate/20200717151209_create_rooms.rb
@@ -2,7 +2,7 @@ class CreateRooms < ActiveRecord::Migration[5.2]
   def change
     create_table :rooms do |t|
       t.string :name
-      t.intger :seating_capacity
+      t.integer :seating_capacity
       t.references :tenant, foreign_key:true
 
       t.timestamps
