8146d0cc19d0209ca85813f937402cfe71ea6e52
khiro <khiro0053@gmail.com>
2020-07-31 14:41:18 +0900

    add validation & test for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  validates :name, presence: true, length: { in: 2..50 }
+  # emailのフォーマットについてはdevice-token-auth側で/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/iと設定されている
+  validates :email, presence: true, uniqueness: true
+  # 半角英小文字大文字数字をそれぞれ1種類以上含む半角英数字記号
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[!-~]+\z/.freeze
+  validates :password, presence: true, length: { in: 8..32 }, format: { with: VALID_PASSWORD_REGEX }, if: :password_required?
+
   has_many :user_tenants, dependent: :destroy
   has_many :tenants, through: :user_tenants
 end
