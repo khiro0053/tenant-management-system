7f3bf373c124f4d39d257d1dacd6442c1b9a3496
khiro <khiro0053@gmail.com>
2020-08-15 21:56:39 +0900

    add API routing about tenants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "auth"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, format: "json" do
+    namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth"
+      resources :tenants
+    end
+  end
 end
