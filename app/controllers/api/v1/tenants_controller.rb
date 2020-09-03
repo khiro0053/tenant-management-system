041fd523f3eedd01c252934bea482c5ea2629415
khiro <khiro0053@gmail.com>
2020-09-04 01:58:25 +0900

    rubocop -a & fix Useless assignment to variable - res

--- app/controllers/api/v1/tenants_controller.rb
+++ app/controllers/api/v1/tenants_controller.rb
@@ -25,11 +25,12 @@ class Api::V1::TenantsController < Api::V1::ApiController
   end
 
   private
-  def set_tenant
-    @tenant = Tenant.find(params[:id])
-  end
 
-  def tenant_params
-    params.require(:tenant).permit(:name, :target_number_of_residents, :capacity, :area_id)
-  end
+    def set_tenant
+      @tenant = Tenant.find(params[:id])
+    end
+
+    def tenant_params
+      params.require(:tenant).permit(:name, :target_number_of_residents, :capacity, :area_id)
+    end
 end
