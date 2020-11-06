e032f8cb86767b8bad2f6a4b7f7beb7e54f3c9bb
khiro <khiro0053@gmail.com>
2020-11-06 22:03:46 +0900

    ウエルカムページのコンポーネント作成

--- app/layouts/welcome.vue
+++ app/layouts/welcome.vue
@@ -1,10 +1,39 @@
 <template>
   <v-app>
-    welcome.vue
+    <app-bar />
+    <v-sheet>
+      <v-container
+        fluid
+        :style="{ maxWidth: '1280px' }"
+      >
+        <v-row
+          v-for="(menu, i) in menus"
+          :key="`menu-${i}`"
+        >
+          <v-col cols="12">
+            <div :is="`${menu.title}`" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-sheet>
   </v-app>
 </template>
 
 <script>
+
+import Products from '~/components/welcome/Products'
+
+
 export default {
+  components: {
+    Products,
+  },
+  data () {
+    return {
+      menus: [
+        { title: 'products', subtitle: '他にはない優れた機能の数々' },
+      ]
+    }
+  }
 }
 </script>
