7aa37ea45b402d1d00f23429107f076fccba7062
khiro <khiro0053@gmail.com>
2020-11-28 23:39:35 +0900

    会員登録ページのレイアウトファイルを作成

--- app/layouts/beforeLogin.vue
+++ app/layouts/beforeLogin.vue
@@ -1,6 +1,14 @@
 <template>
   <v-app>
-  beforeLogin
-   <nuxt />
+    <bef-login-app-bar />
+    <v-main>
+      <nuxt />
+    </v-main>
+    <before-login-footer />
   </v-app>
 </template>
+
+<script>
+export default {
+}
+</script>
