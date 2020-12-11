3d640a217fbcf8cdb91251d98e799fbe7249064a
khiro <khiro0053@gmail.com>
2020-12-11 21:15:29 +0900

    会員登録ページのフォーム作成

--- app/pages/signup.vue
+++ app/pages/signup.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
-    signup.vue
-  </div>
+  <bef-login-card #form-card-content>
+    <v-form
+      v-model="isValid"
+    >
+      <user-form-name />
+      <user-form-email />
+      <user-form-password />
+      <v-btn
+        :disabled="!isValid"
+        block
+        color="myblue"
+        class="white--text"
+      >
+        登録する
+      </v-btn>
+    </v-form>
+  </bef-login-card>
 </template>
 
 <script>
-  export default {
-    layout: 'beforeLogin'
+export default {
+  layout: 'beforeLogin',
+  data () {
+    return {
+      isValid: false,
+      params: { user: { name: '', email: '', password: '' } }
+    }
   }
+}
 </script>
