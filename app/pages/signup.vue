d91d59070e0c16a8b22811ff9e0785c62bdb2766
khiro <khiro0053@gmail.com>
2020-12-19 21:01:54 +0900

    会員登録フォームの親子コンポーネント間のデータ通信

--- app/pages/signup.vue
+++ app/pages/signup.vue
@@ -3,9 +3,15 @@
     <v-form
       v-model="isValid"
     >
-      <user-form-name />
-      <user-form-email />
-      <user-form-password />
+      <user-form-name
+        :name.sync="params.user.name"
+      />
+      <user-form-email
+        :email.sync="params.user.email"
+      />
+      <user-form-password
+        :password.sync="params.user.password"
+      />
       <v-btn
         :disabled="!isValid"
         block
@@ -15,6 +21,9 @@
         登録する
       </v-btn>
     </v-form>
+    <v-card-text>
+      {{ params }}
+    </v-card-text>
   </bef-login-card>
 </template>
 
