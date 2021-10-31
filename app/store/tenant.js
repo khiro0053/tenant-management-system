664c48f71321c3127c684c41db925d458cce6646
khiro <khiro0053@gmail.com>
2021-10-31 16:37:35 +0900

    Vuexに施設情報を更新する処理を追加

--- app/store/tenant.js
+++ app/store/tenant.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 export const state = () => ({
   tenants2: [
     { id: 1, name: 'イエローライフ', capacity: '50' },
@@ -21,6 +22,12 @@ export const getters = {
 export const mutations = {
   setTenants(state, tenants) {
     state.tenants = tenants
+  },
+  setTenant(state, updateTenant) {
+    const id = updateTenant.id
+    const targetTenantID = state.tenants.findIndex((tenant) => tenant.id === id)
+    Vue.set(state.tenants, targetTenantID, updateTenant)
+    console.log(state.tenants[targetTenantID])
   }
 }
 
@@ -30,6 +37,16 @@ export const actions = {
     .then((response) => {
       const tenants = response.data
       commit('setTenants', tenants)
+    })
+  },
+  async tenantsUpdate({commit},data) {
+    await this.$axios.put(`/api/v1/tenants/${data.tenant.id}`, data)
+    .then((response) =>{
+      const tenant = response.data
+      commit('setTenant', tenant)
+    })
+    .catch((error) => {
+      console.log(error)
     })
   }
 }
